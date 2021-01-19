b0b2882fa566dcc7339f75043e642f261ab63bed
Paul Iacomi <pauliacomi@users.noreply.github.com>
2021-01-19 13:47:26 +0100

    remembering color choice

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -1,29 +1,35 @@
-if (window.CSS && CSS.supports("color", "var(--primary)")) {
-    var toggleColorMode = function toggleColorMode(e) {
-      // Switch to Light Mode
-      if (e.currentTarget.classList.contains("light--hidden")) {
-        // Sets the custom html attribute
-        document.documentElement.setAttribute("color-mode", "light"); // Sets the user's preference in local storage
-  
-        localStorage.setItem("color-mode", "light");
-        return;
-      }
-      /* Switch to Dark Mode
-      Sets the custom html attribute */
-      document.documentElement.setAttribute("color-mode", "dark"); // Sets the user's preference in local storage
-  
-      localStorage.setItem("color-mode", "dark");
-    }; // Get the buttons in the DOM
-  
-    var toggleColorButtons = document.querySelectorAll(".color-mode__btn"); // Set up event listeners
-  
-    toggleColorButtons.forEach(function(btn) {
-      btn.addEventListener("click", toggleColorMode);
-    });
-  } else {
-    // If the feature isn't supported, then we hide the toggle buttons
-    var btnContainer = document.querySelector(".color-mode__header");
-    btnContainer.style.display = "none";
-  }
+if (
+  localStorage.getItem("color-mode") === 'light' ||
+  (window.matchMedia('(prefers-color-scheme: light').matches &&
+    !localStorage.getItem('color-mode'))
+) {
+  document.documentElement.setAttribute("color-mode", "light")
+}
 
-console.log("🌑 darkmode loaded")
+if (window.CSS && CSS.supports("color", "var(--primary)")) {
+  var toggleColorMode = function toggleColorMode(e) {
+    // Switch to Light Mode
+    if (e.currentTarget.classList.contains("light--hidden")) {
+      // Sets the custom html attribute
+      document.documentElement.setAttribute("color-mode", "light"); // Sets the user's preference in local storage
+
+      localStorage.setItem("color-mode", "light");
+      return;
+    }
+    /* Switch to Dark Mode
+    Sets the custom html attribute */
+    document.documentElement.setAttribute("color-mode", "dark"); // Sets the user's preference in local storage
+
+    localStorage.setItem("color-mode", "dark");
+  }; // Get the buttons in the DOM
+
+  var toggleColorButtons = document.querySelectorAll(".color-mode__btn"); // Set up event listeners
+
+  toggleColorButtons.forEach(function (btn) {
+    btn.addEventListener("click", toggleColorMode);
+  });
+} else {
+  // If the feature isn't supported, then we hide the toggle buttons
+  var btnContainer = document.querySelector(".color-mode__header");
+  btnContainer.style.display = "none";
+}
