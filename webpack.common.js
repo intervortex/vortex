31728e0ff70461727af6d1279f0b3b55178247be
Paul Iacomi <pauliacomi@users.noreply.github.com>
2021-03-07 01:04:21 +0100

    old file

--- webpack.common.js
+++ webpack.common.js
@@ -31,6 +31,10 @@ module.exports = {
         test: /\.(sa|sc|c)ss$/,
         exclude: /node_modules/,
         use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"]
+      },
+      { 
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+        loader: 'url-loader?limit=100000'
       }
     ]
   },
