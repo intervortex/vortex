1fba170068b7ffd65d98a3e106a9b509b2ed893a
Paul Iacomi <pauliacomi@users.noreply.github.com>
2021-04-26 03:25:16 +0200

    ok modes

--- src/js/cms.js
+++ src/js/cms.js
@@ -2,11 +2,11 @@ import React from "react";
 import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
-import styles from "!css-loader!postcss-loader!sass-loader!../css/main.css";
+import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
 import PostPreview from "./cms-preview-templates/posts";
 
-CMS.registerPreviewStyle(styles.toString(), { raw: true });
+CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("review", PostPreview);
 CMS.registerPreviewTemplate("community", PostPreview);
 CMS.registerPreviewTemplate("blurb", PostPreview);
